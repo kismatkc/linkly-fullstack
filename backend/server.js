003f1f06a8f8dd3fa2d0bb3db67ec7daa94e7280
kismatkc <kismatkc28@gmail.com>
2024-10-08 06:10:26 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api", createUserRoute);
 app.use("/api", authenticateUserRoute);
 app.use("/api", checkUserRoute);
 app.use("/", (req, res) => {
-  res.send("Hello worlb");
+  res.send("Hello worlbmd");
 });
 app.use((err, req, res, next) => {
   console.log(err);
